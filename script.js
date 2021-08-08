580f96c74ecbc27f2f1075bd3b912c1322f4e0bc
dwang <dwang@dwangdeMacBook-Air.local>
2021-08-07 21:44:25 -0700

    fix javascript issue

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-var user = {
-    firstName = this.firstName,
-    lastName = this.lastName,
-    age = this.age,
-    gender = this.gender,
-    fullName = firstName + " " + lastName
-}
+// var user = {
+//     firstName = this.firstName,
+//     lastName = this.lastName,
+//     age = this.age,
+//     gender = this.gender,
+//     fullName = firstName + " " + lastName
+// }
 
-var MelodyWang = {
-    firstName = "Melody",
-    lastName = "Wang",
-    age = 7,
-    gender = "female",
-    fullName = firstName + " " + lastName
-}
+// var MelodyWang = {
+//     firstName = "Melody",
+//     lastName = "Wang",
+//     age = 7,
+//     gender = "female",
+//     fullName = firstName + " " + lastName
+// }
+
+class People {
+    constructor(firstName, lastName, age, gender) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+      this.fullName = firstName + " " + lastName
+    }
+  }
+
+// var MelodyWang = {
+//     firstName: "Melody",
+//     lastName: "Wang",
+//     age: 7,
+//     gender: "female",
+//     fullName: "Melody Wang"
+// }
+
+var MelodyWang = new People("Melody", "Wang", "7", "female")
 
 console.log(MelodyWang.fullName)
